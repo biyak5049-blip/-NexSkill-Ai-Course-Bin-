493e8c0d828fe9ca4f0df9bbc1a0e5253bbced67
biyak5049-blip <biyak5049@gmail.com>
2026-06-16 07:06:58 -0700

    practice for 16 june 2026

---
+++ practice for 16 june 2026.py
@@ -0,0 +1,9 @@
+info = "python is a brilliant programming language"
+print(info)
+print(type(info))
+print(len(info))
+for i in info:
+ print(i)
+
+str = "python is a brilliant programming language"
+print(str[30::37])
