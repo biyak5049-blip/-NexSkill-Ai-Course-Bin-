aa1f977a24ae9389ea734cb0ae56f0089eca9607
biyak5049-blip <biyak5049@gmail.com>
2026-06-22 06:47:18 -0700

    property code

---
+++ practice 22 june 2026.py
@@ -0,0 +1,26 @@
+print("welcome to class 22 june 2026")
+propertyId = 784
+print(propertyId)
+print(type(propertyId))
+propertyprice = 50000.5
+print(propertyprice)
+print(type(propertyprice))
+propertyarea = "gulberg"
+print(propertyarea)
+print(type(propertyarea))
+propertyprice5 = (propertyprice*5)
+print(propertyprice5)
+print(type(propertyprice5))
+propertycity = input("enter your city name")
+print(propertycity)
+print(type(propertycity))
+propertyinfo = input("please enter property info")
+print(propertyinfo)
+print(len(propertyinfo))
+
+for x in propertyinfo: 
+    print(x)
+    
+st = propertyinfo
+print(st[2:4:6])
+
---
+++ practice for 17 june 2026.py
@@ -0,0 +1,21 @@
+BookYear = 1977
+BookPrice = 54.5
+Result = BookYear * BookPrice
+print(BookYear)
+print(BookPrice)
+print(Result)
+print(type(BookYear))
+print(type(BookPrice))
+print(type(Result))
+if BookPrice > 100:
+ print('postive number')
+elif BookPrice < 100:
+ print('postive number')
+ 
+else:
+ print('do not buy')
+
+print('this statement is always executed')
+
+
+
